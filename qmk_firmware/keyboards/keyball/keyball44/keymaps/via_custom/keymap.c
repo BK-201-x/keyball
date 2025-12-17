e06dd431426bbc0cc46877b37e168e5111a6715d
タスク <g08.barbatoslupus@gmail.com>
2025-12-17 11:41:41 +0900

    no message

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/keymap.c
@@ -1,16 +1,37 @@
 #include QMK_KEYBOARD_H
 #include "quantum.h"
-#include "tap_dance.h"
+#include "tap_dance.h" // Tap Dance機能のインクルード
 
+// ---------------------------------------------------------
+// ■ 1. グローバル定義（トップレベル）
+// ---------------------------------------------------------
+
+// Tap Dance 識別子（TD()で使われている定数をここで定義）
 enum {
   TD_KANA_EISU,
 };
 
-// --- ★追加: マウスロック状態管理フラグ ---
+// コンボイベント識別子
+enum combo_events {
+  // ご要望のドラッグ・トグル用
+  KL_BTN1, // K + L -> Btn1
+  LS_BTN2, // L + ; -> Btn2 (L + : も兼ねる)
+  
+  // 既存のコンボ
+  QW_ESC,
+  MY_F7,
+  YU_F8,
+};
+
+// マウスボタンのロック状態（ドラッグ状態）を管理するフラグ
 static bool is_btn1_locked = false;
 static bool is_btn2_locked = false;
 
-// --- ★追加: マウスボタンのトグル関数 ---
+// ---------------------------------------------------------
+// ■ 2. カスタム関数定義（トップレベル）
+// ---------------------------------------------------------
+
+// --- Btn1 トグル関数 ---
 void toggle_mouse_btn1(void) {
   is_btn1_locked = !is_btn1_locked;
   if (is_btn1_locked) {
@@ -20,6 +41,7 @@ void toggle_mouse_btn1(void) {
   }
 }
 
+// --- Btn2 トグル関数 ---
 void toggle_mouse_btn2(void) {
   is_btn2_locked = !is_btn2_locked;
   if (is_btn2_locked) {
@@ -29,34 +51,180 @@ void toggle_mouse_btn2(void) {
   }
 }
 
+// --- Tap Dance コールバック関数 ---
+void kana_eisu_finished(tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    tap_code(KC_LNG1); // かな
+  } else if (state->count == 2) {
+    tap_code(KC_LNG2); // 英数
+  }
+}
+
+
+// ---------------------------------------------------------
+// ■ 3. 配列定義（トップレベル）
+// ---------------------------------------------------------
+
+// --- Tap Dance アクション配列 ---
+tap_dance_action_t tap_dance_actions[] = {
+          [TD_KANA_EISU] = ACTION_TAP_DANCE_FN(kana_eisu_finished),
+        };
+
+// --- コンボキー配列 ---
+#ifdef COMBO_ENABLE
+// コンボキーの組み合わせ定義
+const uint16_t PROGMEM combo_kl[] = {KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM combo_ls[] = {KC_L, KC_SCLN, COMBO_END};
+const uint16_t PROGMEM combo_qw[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_my[] = {KC_MINS, KC_Y, COMBO_END};
+const uint16_t PROGMEM combo_yu[] = {KC_Y, KC_U, COMBO_END};
+
+// コンボアクションへの割り当て（配列の終わりを示す COMBO_END/NULL は不要）
+combo_t key_combos[] = {
+          [KL_BTN1] = COMBO_ACTION(combo_kl),
+          [LS_BTN2] = COMBO_ACTION(combo_ls),
+          
+          [QW_ESC] = COMBO_ACTION(combo_qw),
+          [MY_F7]  = COMBO_ACTION(combo_my),
+          [YU_F8]  = COMBO_ACTION(combo_yu),
+        };
+#endif
+
+
+// ---------------------------------------------------------
+// ■ 4. QMKフック関数
+// ---------------------------------------------------------
+
+// --- コンボイベント処理 ---
+#ifdef COMBO_ENABLE
+void process_combo_event(uint16_t combo_index, bool pressed) {
+  if (pressed) {
+    switch (combo_index) {
+      
+      // --- ドラッグ・トグル用コンボ ---
+    case KL_BTN1: // • K + L -> Btn1 (トグル)
+      toggle_mouse_btn1();
+      break;
+    case LS_BTN2: // • L + ; -> Btn2 (トグル)
+      // • Btn1 + Btn2 -> Btn2 のロジックもここでカバー
+      toggle_mouse_btn2();
+      break;
+      
+      // --- 既存の単発キーコンボ ---
+    case QW_ESC:
+      tap_code(KC_ESC);
+      break;
+    case MY_F7:
+      tap_code(KC_F7);
+      break;
+    case YU_F8:
+      tap_code(KC_F8);
+      break;
+    }
+  }
+}
+#endif
+
+// --- ユーザー入力処理（特殊ロジック） ---
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  
+  // --------------------------------------------------
+  // ご要望の特殊ロジック処理
+  // --------------------------------------------------
+  if (record->event.pressed) {
+    
+    // • 9 + Btn1 -> Btn1 (トグル)
+    if (keycode == KC_9) {
+      // Btn1がロック中ならトグル（解除/再ロック）
+      if (is_btn1_locked) {
+        toggle_mouse_btn1();
+        // 9の入力自体はキャンセルしない (移動も継続させるため)
+      }
+    }
+    
+    // • 6 + Btn1 -> Btn1 (トグル)
+    if (keycode == KC_6) {
+      if (is_btn1_locked) {
+        toggle_mouse_btn1();
+      }
+    }
+    
+    // • Btn1 + Btn2 -> Btn2 の特殊処理は process_combo_event でカバー済
+    // (Btn1ロック中にBtn2コンボを押した場合)
+    
+  }
+  
+  // --------------------------------------------------
+  // 既存の Keyball スクロール処理
+  // --------------------------------------------------
+  switch (keycode) {
+  case KC_BSPC:
+    if (layer_state_is(0) || layer_state_is(1)) {
+      if (record->event.pressed) {
+        keyball_set_scroll_mode(true);
+      } else {
+        // レイヤー3でなければ戻す
+        if (get_highest_layer(layer_state) != 3) {
+          keyball_set_scroll_mode(false);
+        }
+      }
+    }
+    return true;
+  }
+  
+  return true;
+}
+
+// --- レイヤーステート処理 ---
+layer_state_t layer_state_set_user(layer_state_t state) {
+  // Auto enable scroll mode when the highest layer is 3
+  keyball_set_scroll_mode(get_highest_layer(state) == 3);
+  return state;
+}
+
+#ifdef OLED_ENABLE
+#include "lib/oledkit/oledkit.h"
+void oledkit_render_info_user(void) {
+  keyball_oled_render_keyinfo();
+  keyball_oled_render_ballinfo();
+  keyball_oled_render_layerinfo();
+}
+#endif
+
+
+// ---------------------------------------------------------
+// ■ 5. キーマップ定義（レイアウト）
+// ---------------------------------------------------------
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           // keymap for default (VIA)
           [0] = LAYOUT_universal(
-            KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                         KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
-            KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                         KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
-            KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                         KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1  ,
-            KC_LALT,KC_LGUI,LCTL_T(KC_LNG2)     ,LT(1,KC_SPC),LT(3, TD(TD_KANA_EISU)),                  KC_BSPC,LT(2,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
+            KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
+            KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
+            KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                  KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1  ,
+            KC_LALT,KC_LGUI,LCTL_T(KC_LNG2)     ,LT(1,KC_SPC),LT(3, TD(TD_KANA_EISU)),                   KC_BSPC,LT(2,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
             ),
           [1] = LAYOUT_universal(
-            SSNP_FRE ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                          KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-            SSNP_VRT ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                          KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_F12   ,
-            SSNP_HOR ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                          KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
-            _______  , _______ , _______  ,         _______  , _______  ,                  _______  , _______  , _______       , _______  , _______
+            SSNP_FRE ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                   KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
+            SSNP_VRT ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                   KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_F12   ,
+            SSNP_HOR ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                   KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
+            _______  , _______ , _______  ,          _______  , _______  ,                   _______  , _______  , _______         , _______  , _______
             ),
           [2] = LAYOUT_universal(
-            _______  ,S(KC_QUOT), KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                          S(KC_9)  , S(KC_1)  , S(KC_6)  , KC_LBRC  , S(KC_4)  , _______  ,
-            _______  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_RBRC  ,                                          KC_NUHS  , KC_MINS  , S(KC_EQL), S(KC_3)  , KC_QUOT  , S(KC_2)  ,
-            _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                         S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH),S(KC_INT3),
-            KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                  KC_DEL   , _______  , _______       , _______  , _______
+            _______  ,S(KC_QUOT), KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                   S(KC_9)  , S(KC_1)  , S(KC_6)  , KC_LBRC  , S(KC_4)  , _______  ,
+            _______  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_RBRC  ,                                   KC_NUHS  , KC_MINS  , S(KC_EQL), S(KC_3)  , KC_QUOT  , S(KC_2)  ,
+            _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                  S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH),S(KC_INT3),
+            KC_0     , KC_DOT  , _______  ,          _______  , _______  ,                   KC_DEL   , _______  , _______         , _______  , _______
             ),
           [3] = LAYOUT_universal(
-            RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,                                          RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
-            RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                          RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
-            RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                          CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
-            QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                  _______  , _______  , _______       , KBC_RST  , QK_BOOT
+            RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,                                   RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
+            RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                   RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
+            RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                   CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
+            QK_BOOT  , KBC_RST  , _______  ,          _______  , _______  ,                   _______  , _______  , _______         , KBC_RST  , QK_BOOT
             ),
         };
+
 // clang-format on
 
 void kana_eisu_finished(tap_dance_state_t *state, void *user_data) {

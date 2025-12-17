71f936b5e4fd7b2ef4082c2f65c88239f1c84baa
タスク <g08.barbatoslupus@gmail.com>
2025-12-17 17:12:30 +0900

    no message

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_TAP_DANCE/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_TAP_DANCE/keymap.c
@@ -1,26 +1,22 @@
 #include QMK_KEYBOARD_H
-#include "quantum.h"
 
-/* ─────────────────────────────
- * Tap Dance 定義
- * ───────────────────────────── */
+// Tap Dance 関連は一切使わない
+// #include "tap_dance.h" ← 絶対に書かない
 
-enum {
-  TD_KANA_EISU,
+enum layer_names {
+  _BASE,
+  _LOWER,
+  _RAISE,
+  _ADJUST
 };
 
-/* Tap Dance 動作テーブル */
-tap_dance_action_t tap_dance_actions[] = {
-          [TD_KANA_EISU] = ACTION_TAP_DANCE_DOUBLE(KC_LNG2, KC_LNG1),
-        };
-
-// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
           
           /* =========================================================
    * Layer 0 : Default
    * ========================================================= */
-          [0] = LAYOUT_keyball44(
+          [_BASE] = LAYOUT_no_ball(
             KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
             KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
             KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1  ,
@@ -31,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           /* =========================================================
    * Layer 1
    * ========================================================= */
-          [1] = LAYOUT_keyball44(
+          [_LOWER] = LAYOUT_no_ball(
             SSNP_FRE , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   ,                                       KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11 ,
             SSNP_VRT , _______ , _______ , KC_UP   , KC_ENT  , KC_DEL  ,                                       KC_PGUP , KC_BTN1 , KC_UP   , KC_BTN2 , KC_BTN3 , KC_F12 ,
             SSNP_HOR , _______ , KC_LEFT , KC_DOWN , KC_RGHT , KC_BSPC ,                                       KC_PGDN , KC_LEFT , KC_DOWN , KC_RGHT , _______ , _______,
@@ -41,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           /* =========================================================
    * Layer 2
    * ========================================================= */
-          [2] = LAYOUT_keyball44(
+          [_RAISE] = LAYOUT_no_ball(
             _______ , S(KC_QUOT), KC_7 , KC_8 , KC_9 , S(KC_8) ,                                    S(KC_9) , S(KC_1) , S(KC_6) , KC_LBRC , S(KC_4) , _______,
             _______ , S(KC_SCLN), KC_4 , KC_5 , KC_6 , KC_RBRC ,                                    KC_NUHS , KC_MINS , S(KC_EQL), S(KC_3) , KC_QUOT , S(KC_2),
             _______ , S(KC_MINS), KC_1 , KC_2 , KC_3 , S(KC_RBRC),                                   S(KC_NUHS), S(KC_INT1), KC_EQL , S(KC_LBRC), S(KC_SLSH), S(KC_INT3),
@@ -51,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           /* =========================================================
    * Layer 3
    * ========================================================= */
-          [3] = LAYOUT_keyball44(
+          [_ADJUST] = LAYOUT_no_ball(
             RGB_TOG , AML_TO , AML_I50 , AML_D50 , _______ , _______ ,                              RGB_M_P , RGB_M_B , RGB_M_R , RGB_M_SW , RGB_M_SN , RGB_M_K ,
             RGB_MOD , RGB_HUI, RGB_SAI , RGB_VAI , _______ , SCRL_DVI,                              RGB_M_X , RGB_M_G , RGB_M_T , RGB_M_TW , _______ , _______,
             RGB_RMOD, RGB_HUD, RGB_SAD , RGB_VAD , _______ , SCRL_DVD,                              CPI_D1K , CPI_D100, CPI_I100, CPI_I1K , _______ , KBC_SAVE,

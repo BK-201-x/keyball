7e01d71b2c93f72d312d01f72e75e25ff778d924
タスク <g08.barbatoslupus@gmail.com>
2025-12-21 13:20:33 +0900

    no message

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_TAP_DANCE/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_TAP_DANCE/keymap.c
@@ -14,7 +14,7 @@ void kana_eisu_finished(tap_dance_state_t *state, void *user_data) {
 }
 
 tap_dance_action_t tap_dance_actions[] = {
-          [TD_KANA_EISU] = ACTION_TAP_DANCE_FN(NULL, kana_eisu_finished),
+          [TD_KANA_EISU] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, kana_eisu_finished, NULL),
         };
 
 // =============================

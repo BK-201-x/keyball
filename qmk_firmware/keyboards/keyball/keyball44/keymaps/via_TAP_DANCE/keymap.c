30c0fbd03569bf82afec259ab20c1191f541a1d3
タスク <g08.barbatoslupus@gmail.com>
2025-12-21 14:20:29 +0900

    no message

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_TAP_DANCE/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_TAP_DANCE/keymap.c
@@ -1,104 +1,43 @@
-#define TAPPING_TERM 300
-
 #include QMK_KEYBOARD_H
 
-// =====================================================
-// Custom keycodes
-// =====================================================
-enum custom_keycodes {
-  KANA_EISU = SAFE_RANGE,
+// --------------------------------------------------
+// Tap Dance 定義
+// --------------------------------------------------
+
+enum {
+  TD_KANA_EISU = 0,
 };
 
-// =====================================================
-// KANA / EISU tap-count state
-// =====================================================
-static uint16_t kana_timer = 0;
-static uint8_t  kana_count = 0;
+// 1回: かな / 2回以上: 英数
+void kana_eisu_finished(tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    tap_code(KC_LNG1);   // かな
+  } else {
+    tap_code(KC_LNG2);   // 英数（安全側）
+  }
+}
 
-// =====================================================
+tap_dance_action_t tap_dance_actions[] = {
+          [TD_KANA_EISU] = ACTION_TAP_DANCE_FN(kana_eisu_finished),
+        };
+
+// --------------------------------------------------
 // Keymaps
-// =====================================================
+// --------------------------------------------------
 
-// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           
-          [0] = LAYOUT_universal(
-            KC_ESC , KC_Q , KC_W , KC_E , KC_R , KC_T ,                         KC_Y , KC_U , KC_I , KC_O , KC_P , KC_DEL ,
-            KC_TAB , KC_A , KC_S , KC_D , KC_F , KC_G ,                         KC_H , KC_J , KC_K , KC_L , KC_SCLN, S(KC_7),
-            KC_LSFT, KC_Z , KC_X , KC_C , KC_V , KC_B ,                         KC_N , KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_INT1,
-            KC_LALT, KC_LGUI, LCTL_T(KC_LNG2), LT(1,KC_SPC),
-            KANA_EISU,
-            KC_BSPC, LT(2,KC_ENT), RCTL_T(KC_LNG2), KC_RALT, KC_PSCR
+          [0] = LAYOUT(
+            // ---------------- Left ----------------
+            KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+            KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+            KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+            TD(TD_KANA_EISU), KC_LALT,   // ★ここが かな／英数
+            
+            // ---------------- Right ----------------
+            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+            KC_SPC,  KC_RALT
             ),
-          
-          [1] = LAYOUT_universal(
-            SSNP_FRE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                       KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-            SSNP_VRT, _______, _______, KC_UP, KC_ENT, KC_DEL,               KC_PGUP, KC_BTN1, KC_UP, KC_BTN2, KC_BTN3, KC_F12,
-            SSNP_HOR, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,            KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-            _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______
-            ),
-          
-          [2] = LAYOUT_universal(
-            _______, S(KC_QUOT), KC_7, KC_8, KC_9, S(KC_8),                    S(KC_9), S(KC_1), S(KC_6), KC_LBRC, S(KC_4), _______,
-            _______, S(KC_SCLN), KC_4, KC_5, KC_6, KC_RBRC,                    KC_NUHS, KC_MINS, S(KC_EQL), S(KC_3), KC_QUOT, S(KC_2),
-            _______, S(KC_MINS), KC_1, KC_2, KC_3, S(KC_RBRC),                 S(KC_NUHS), S(KC_INT1), KC_EQL, S(KC_LBRC), S(KC_SLSH), S(KC_INT3),
-            KC_0, KC_DOT, _______, _______, _______,                          KC_DEL, _______, _______, _______, _______
-            ),
-          
-          [3] = LAYOUT_universal(
-            RGB_TOG, AML_TO, AML_I50, AML_D50, _______, _______,               RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K,
-            RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, SCRL_DVI,             RGB_M_X, RGB_M_G, RGB_M_T, RGB_M_TW, _______, _______,
-            RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, SCRL_DVD,            CPI_D1K, CPI_D100, CPI_I100, CPI_I1K, _______, KBC_SAVE,
-            QK_BOOT, KBC_RST, _______, _______, _______,                      _______, _______, _______, KBC_RST, QK_BOOT
-            ),
-          
         };
-// clang-format on
-
-// =====================================================
-// Layer hook
-// =====================================================
-layer_state_t layer_state_set_user(layer_state_t state) {
-  keyball_set_scroll_mode(get_highest_layer(state) == 3);
-  return state;
-}
-
-// =====================================================
-// KANA / EISU multi-tap logic (VIA safe)
-// =====================================================
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (keycode == KANA_EISU) {
-    if (record->event.pressed) {
-      if (timer_elapsed(kana_timer) > TAPPING_TERM) {
-        kana_count = 0;
-      }
-      kana_timer = timer_read();
-      kana_count++;
-    }
-    return false;
-  }
-  return true;
-}
-
-void matrix_scan_user(void) {
-  if (kana_count > 0 && timer_elapsed(kana_timer) > TAPPING_TERM) {
-    if (kana_count == 1) {
-      tap_code(KC_LNG1); // かな
-    } else {
-      tap_code(KC_LNG2); // 英数（2回以上・連打含む）
-    }
-    kana_count = 0;
-  }
-}
-
-// =====================================================
-// OLED
-// =====================================================
-#ifdef OLED_ENABLE
-#include "lib/oledkit/oledkit.h"
-void oledkit_render_info_user(void) {
-  keyball_oled_render_keyinfo();
-  keyball_oled_render_ballinfo();
-  keyball_oled_render_layerinfo();
-}
-#endif
